95bcc35cfca19b53efbf6aab406b271102eecb65
Matthias Holetzko <mholetzko@gmx.net>
2021-07-28 23:18:26 +0200

    feat: new layout

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,26 +2,27 @@
 import React, { useState } from "react";
 import { useAsync } from "react-async";
 import { fetchFood } from "./FoodApi";
+import Logo from "../logo.png"
 
 const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
 ];
 interface IApiSample {
-  name: String;
-  type: String;
-  harvest_season: [String];
-  storage_season: [String];
+    name: String;
+    type: String;
+    harvest_season: [String];
+    storage_season: [String];
 }
 
 let dateObj = new Date();
@@ -29,200 +30,231 @@ let month = dateObj.getUTCMonth();
 const currentMonth: String = monthNames[month];
 
 const Content = () => {
-  const [showHarvest, setShowHarvest] = useState<boolean>(true);
-  const [showFruits, setshowFruits] = useState<boolean>(true);
-  const [showSalat, setshowSalat] = useState<boolean>(true);
-  const [showVegetables, setshowVegetables] = useState<boolean>(true);
-  const [reSyncApi, setreSyncApi] = useState<boolean>(false);
+    const [showHarvest, setShowHarvest] = useState<boolean>(true);
+    const [showFruits, setshowFruits] = useState<boolean>(true);
+    const [showSalat, setshowSalat] = useState<boolean>(true);
+    const [showVegetables, setshowVegetables] = useState<boolean>(true);
+    const [Veggie, setVeggie] = useState<string>("✅ ");
+    const [Fruit, setFruit] = useState<string>("✅ ");
+    const [Salat, setSalat] = useState<string>("✅ ");
+    const [reSyncApi, setreSyncApi] = useState<boolean>(false);
 
-  const buildTableHeader = () => {
-    return (
-      <>
-        <div className="one">Name</div>
-        <div className="two">Typ</div>
-        <div className="three">Kaufen</div>
-      </>
-    );
-  };
-
-  const buildTableBody = (pending: boolean, data: any, error: any) => {
-    if (pending) {
-      return (
-        <div className="grid_wrapper">
-          <div className="one">Loading data ...</div>
-        </div>
-      );
-    } else if (error) {
-      return (
-        <div className="grid_wrapper">
-          <div className="one">Could not fetch API ...</div>
-        </div>
-      );
-    } else {
-      let input: [IApiSample] = data["data"]["food_by_month"];
-
-      return (
-        <>
-          <div className="grid_wrapper_heading">{buildTableHeader()}</div>
-          <div className="grid_wrapper">
-            {input.map((foodArr) => {
-              if (foodArr["type"] === "Fruit" && !showFruits) {
-                return <> </>;
-              }
-              if (foodArr["type"] === "Vegetable" && !showVegetables) {
-                return <> </>;
-              }
-              if (foodArr["type"] === "Salat" && !showSalat) {
-                return <> </>;
-              }
-              return (
-                <>
-                  <div className="one">{foodArr["name"]}</div>
-                  <div className="two">{foodArr["type"]}</div>
-                  <div className="three">Buy at a random store</div>
-                </>
-              );
-            })}
-          </div>
-        </>
-      );
+    const getClassFromFoodType = (type:String):string=>{
+         if(type === "Salat"){
+             return "App-card-body-fruit"
+         } else if(type === "Vegetable") {
+             return "App-card-body-fruit"
+         } else {
+             return "App-card-body-fruit"
+         }
     }
-  };
 
-  let { data, error, isPending } = useAsync({
-    promiseFn: fetchFood,
-    harvest: showHarvest,
-    watch: reSyncApi,
-  });
+    const buildTableBody = (pending: boolean, data: any, error: any) => {
+        if (pending) {
+            return (
+                <div className="grid_wrapper">
+                    <div className="one">Loading data ...</div>
+                </div>
+            );
+        } else if (error) {
+            return (
+                <div className="grid_wrapper">
+                    <div className="one">Could not fetch API ...</div>
+                </div>
+            );
+        } else {
+            let input: [IApiSample] = data["data"]["food_by_month"];
 
-  const setStatusVegetables = () => {
-    setshowVegetables(!showVegetables);
-    setreSyncApi(!reSyncApi);
-  };
-  const setStatusFruits = () => {
-    setshowFruits(!showFruits);
-    setreSyncApi(!reSyncApi);
-  };
-  const setStatusSalat = () => {
-    setshowSalat(!showSalat);
-    setreSyncApi(!reSyncApi);
-  };
+            return (
+                <>
+                        {input.map((foodArr) => {
+                            if (foodArr["type"] === "Fruit" && !showFruits) {
+                                return <> </>;
+                            }
+                            if (foodArr["type"] === "Vegetable" && !showVegetables) {
+                                return <> </>;
+                            }
+                            if (foodArr["type"] === "Salat" && !showSalat) {
+                                return <> </>;
+                            }
+                            return (
+                                <>
+                                <div className="col-md-3 pb-1 pb-md-0">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <img justify-content="center" width="20%" src={Logo} alt="Card image cap" className="rounded" />
+                                        </div>
+                                        <div className={getClassFromFoodType(foodArr["type"])}>
+                                            <h5 className="card-title">{foodArr["name"]}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                </>
+                            );
+                        })}
+                </>
+            );
+        }
+    };
 
-  const setStatusHarvest = () => {
-    setShowHarvest(true);
-    setreSyncApi(!reSyncApi);
-    document
-      .getElementById("showStorage")
-      ?.classList.remove("food_info__tab--active");
-    document
-      .getElementById("showHarvest")
-      ?.classList.add("food_info__tab--active");
-  };
+    let { data, error, isPending } = useAsync({
+        promiseFn: fetchFood,
+        harvest: showHarvest,
+        watch: reSyncApi,
+    });
 
-  const setStatusStorage = () => {
-    setShowHarvest(false);
-    setreSyncApi(!reSyncApi);
-    document
-      .getElementById("showHarvest")
-      ?.classList.remove("food_info__tab--active");
-    document
-      .getElementById("showStorage")
-      ?.classList.add("food_info__tab--active");
-  };
+    const setStatusVegetables = () => {
+        setshowVegetables(!showVegetables);
+        if (!showVegetables) {
+            document
+                .getElementById("veggie-button")
+                ?.classList.add("active");
+                setVeggie("✅ ")
+        } else {
+            document
+                .getElementById("veggie-button")
+                ?.classList.remove("active");
+                setVeggie("❎ ")
+        }
+        setreSyncApi(!reSyncApi);
 
-  return (
-    <main className="content">
-      <section className="section__wrapper" id="section--food--list">
-        <div className="section__title">
-          <h2 className="section__description">Es ist {currentMonth}</h2>
-          <h3 className="section__header">
-            Übersicht für den aktuellen Monat
-            <br />
-            Saisonal & Regional sind
-          </h3>
-          <br />
-          <div className="food_info__tab-container">
-            <label className="switch">
-              <input
-                className="switch-input"
-                type="checkbox"
-                id="toggleVegetables"
-                onChange={setStatusVegetables}
-              />
-              <span
-                className="switch-label"
-                data-on="Gemüse"
-                data-off="Gemüse"
-              ></span>
-              <span className="switch-handle"></span>
-            </label>
-            <label className="switch">
-              <input
-                className="switch-input"
-                type="checkbox"
-                id="toggleSalat"
-                onChange={setStatusSalat}
-              />
-              <span
-                className="switch-label"
-                data-on="Salat"
-                data-off="Salat"
-              ></span>
-              <span className="switch-handle"></span>
-            </label>
-            <label className="switch">
-              <input
-                className="switch-input"
-                type="checkbox"
-                id="toggleFruit"
-                onChange={setStatusFruits}
-              />
-              <span
-                className="switch-label"
-                data-on="Früchte"
-                data-off="Früchte"
-              ></span>
-              <span className="switch-handle"></span>
-            </label>
-          </div>
-        </div>
-        <div className="food_info">
-          <div className="food_info__tab-container">
-            <button
-              className="btn food_info__tab food_info__tab--1 food_info__tab--active"
-              id="showHarvest"
-              onClick={setStatusHarvest}
-            >
-              Frisch vom Feld
-            </button>
-            <button
-              className="btn food_info__tab food_info__tab--2"
-              id="showStorage"
-              onClick={setStatusStorage}
-            >
-              Aus dem Lager
-            </button>
-          </div>
-          {buildTableBody(isPending, data, error)}
-        </div>
-      </section>
-      <section className="section__wrapper" id="section--about">
-        <div className="section__wrapped">
-          <h2 className="section__description">About</h2>
-          <h3 className="section__header">...</h3>
-        </div>
-        <div className="section__wrapped">
-          <h2 className="section__description">How it started</h2>
-          <h3 className="section__header">...</h3>
-        </div>
-      </section>
-      <section className="section__wrapper" id="section--api-doc">
-        <div className="section__wrapped">
-          <h2 className="section__description">API documentation</h2>
-          <h3 className="section__header">...</h3>
-        </div>
-      </section>
-    </main>
-  );
+    };
+    const setStatusFruits = () => {
+        setshowFruits(!showFruits);
+        if (!showFruits) {
+            document
+                .getElementById("fruit-button")
+                ?.classList.add("active");
+                setFruit("✅ ")
+        } else {
+            document
+                .getElementById("fruit-button")
+                ?.classList.remove("active");
+                setFruit("❎ ")
+        }
+        setreSyncApi(!reSyncApi);
+    };
+    const setStatusSalat = () => {
+        setshowSalat(!showSalat);
+        if (!showSalat) {
+            document
+                .getElementById("salat-button")
+                ?.classList.add("active");
+                setSalat("✅ ")
+        } else {
+            document
+                .getElementById("salat-button")
+                ?.classList.remove("active");
+                setSalat("❎ ")
+        }
+        setreSyncApi(!reSyncApi);
+    };
+
+    const setStatusHarvest = () => {
+        setShowHarvest(true);
+        setreSyncApi(!reSyncApi);
+        document
+            .getElementById("showStorage")
+            ?.classList.remove("food_info__tab--active");
+        document
+            .getElementById("showHarvest")
+            ?.classList.add("food_info__tab--active");
+    };
+
+    const setStatusStorage = () => {
+        setShowHarvest(false);
+        setreSyncApi(!reSyncApi);
+        document
+            .getElementById("showHarvest")
+            ?.classList.remove("food_info__tab--active");
+        document
+            .getElementById("showStorage")
+            ?.classList.add("food_info__tab--active");
+    };
+
+    return (
+        <main className="content">
+            <section>
+                <div className="jumbotron text-center mt-2">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h1>Es ist {currentMonth}</h1>
+                                <p>            Übersicht für den aktuellen Monat
+                                    <br />
+                                    Saisonal & Regional sind</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section__title">
+                    <div className="container">
+                        <div className="row text-center">
+                            
+                            <div className="col-md-4 pb-1 pb-md-0">
+                                <button  type="button" onClick={setStatusVegetables} id="veggie-button" className="btn btn-outline-success active"  >{Veggie}Vegetables</button></div>
+                            <div className="col-md-4 pb-1 pb-md-0">
+                                <button type="button" onClick={setStatusFruits} id="fruit-button" className="btn btn-outline-success active"  >{Fruit}Fruit</button></div>
+                            <div className="col-md-4 pb-1 pb-md-0">
+                                <button type="button" onClick={setStatusSalat} id="salat-button" className="btn btn-outline-success active"  >{Salat}Salat</button></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="food_info">
+                    <div className="food_info__tab-container">
+                        <button
+                            className="btn food_info__tab food_info__tab--1 food_info__tab--active"
+                            id="showHarvest"
+                            onClick={setStatusHarvest}
+                        >
+                            Frisch vom Feld
+                        </button>
+                        <button
+                            className="btn food_info__tab food_info__tab--2"
+                            id="showStorage"
+                            onClick={setStatusStorage}
+                        >
+                            Aus dem Lager
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="App-pre-scrollable"> 
+                <div className="App-container-fluid">
+                    <div className="row text-center mt-4">
+                        {buildTableBody(isPending, data, error)}
+                    </div>
+                </div>
+            </section>
+            <section className="section__wrapper" id="section--about">
+                <div className="jumbotron text-center mt-2">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h1>About</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section__wrapped">
+                    <h3 className="App-section">...</h3>
+                </div>
+            </section>
+            <section className="section__wrapper" id="section--api-doc">
+                <div className="jumbotron text-center mt-2">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h1>API documentation</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section__wrapped">
+                    <h3 className="App-section">...</h3>
+                </div>
+            </section>
+        </main>
+    );
 };
 export default Content;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,36 +4,25 @@ import Logo from "./../logo.png";
 const Navbar = () => {
   return (
     <header>
-      <nav className="nav">
-        <img src={Logo} alt="SPRLogo" className="nav__logo" />
-        <ul className="nav__links">
-          <li className="nav__item">
-            <a className="nav__link" href="#section--food--list">
-              Food List
-            </a>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <a className="navbar-brand" href="#">srp711</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="section--food--list">Food List <span className="sr-only">(current)</span></a>
           </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#section--about">
-              About
-            </a>
+          <li className="nav-item">
+            <a className="nav-link" href="#section--about">About</a>
           </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#section--api-doc">
-              Api
-            </a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link nav__link--btn btn--show-modal" href="#">
-              Add Store
-            </a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link nav__link--btn btn--show-modal" href="#">
-              Add Food
-            </a>
+          <li className="nav-item">
+            <a className="nav-link" href="#section--api-doc">API</a>
           </li>
         </ul>
-      </nav>
+      </div>
+    </nav>
     </header>
   );
 };
